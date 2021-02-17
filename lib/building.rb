b561da3485fdaa53d9f2ceca78f2ae8060cd7cf3
Alex Klick <aklick12@gmail.com>
2021-02-17 11:00:55 -0700

    units_by_number_of_bedrooms, renter_with_highest_rent

--- lib/building.rb
+++ lib/building.rb
@@ -1,8 +1,9 @@
 class Building
-  attr_reader :units, :renters
+  attr_reader :units, :renters, :rented_units
   def initialize
     @units = []
     @renters = []
+    @rented_units = []
   end
 
   def add_unit(unit)
@@ -13,6 +14,29 @@ class Building
     @renters.map {|renter| renter.name}
   end
   def average_rent
-    @units.map {|unit| unit.monthly_rent}.sum(0.0) / @units.size
+    @units.map {|unit| unit.monthly_rent}.sum(0.0) / @units.length
+  end
+
+  def rented_units
+    @units.map {|unit| unit.renter ? unit : nil}.compact
+  end
+
+  def renter_with_highest_rent
+    highest_rent = 0
+    renter = rented_units.map {|unit| unit.monthly_rent > highest_rent ? unit.renter : nil}.compact
+    renter[0]
+  end
+
+  def units_by_number_of_bedrooms
+    bedrooms = Hash.new([])
+    @units.map { 
+      |unit| 
+      if bedrooms[unit.bedrooms].length > 0
+        bedrooms[unit.bedrooms]  << unit.number 
+      else
+        bedrooms[unit.bedrooms] = [unit.number]
+      end
+    }
+    bedrooms
   end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -20,13 +20,12 @@ class BuildingTest < Minitest::Test
     assert_equal [], @building.units
     @building.add_unit(@unit1)
     @building.add_unit(@unit2)
-    assert_equal building.units, [@unit1,@unit2]
+    assert_equal @building.units, [@unit1,@unit2]
   end
 
   def test_can_add_renter
     @building.add_unit(@unit1)
     @building.add_unit(@unit2)
-    #require 'pry'; binding.pry
     assert_equal @building.renters, []
     renter1 = Renter.new("Aurora")
     @unit1.add_renter(renter1)
@@ -42,8 +41,35 @@ class BuildingTest < Minitest::Test
     @unit1.add_renter(renter1)
     renter2 = Renter.new("Tim")
     @unit1.add_renter(renter2)
-    assert_equal building.average_rent, 1099.5
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    assert_equal @building.average_rent, 1099.5
   end
 
-
+  def test_full_test
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    assert_equal building.rented_units, []
+    unit2.add_renter(renter1)
+    assert_equal building.rented_units, [unit2]
+    assert_equal renter1, building.renter_with_highest_rent
+    renter2 = Renter.new("Jessie")
+    unit1.add_renter(renter2)
+    assert_equal renter2, building.renter_with_highest_rent
+    renter3 = Renter.new("Max")
+    unit3.add_renter(renter3)
+    assert_equal renter2, building.renter_with_highest_rent
+    building.add_unit(unit4)
+    require 'pry'; binding.pry
+    assert_equal building.units_by_number_of_bedrooms, {3 => ["D4" ],
+2 => ["B2", "C3"],
+1 => ["A1"]}
+  end
 end
